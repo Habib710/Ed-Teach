f89e8972432c244b2f46f06eacc326cf7d0b3264
habib <mdhabib71024@gmail.com>
2022-08-20 16:32:56 +0600

    another section added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,10 +12,10 @@ import {
   faFilm,
   faChartLine,
   faCertificate,
-  faBook
+  faBook,
 } from '@fortawesome/free-solid-svg-icons'
 import './Home.css';
-
+import pp from '../../img/testi_01.png'
 
 const Home = () => {
   const [services, setservices] = useState([])
@@ -68,15 +68,15 @@ const Home = () => {
       </section>
       <section className="container py-5">
         <h2 className="text-center py-4 ">OUR SERVICES</h2>
-        <hr className='w-25 m-auto mb-5 c-tomato fw-2' />
+        <hr className="w-25 m-auto mb-5 c-tomato fw-2" />
         <div className="Card-css">
           {services.map((service) => (
-            <Card className='card-size'>
-              <Card.Img variant="top" height="190px"  src={service.pic} />
+            <Card className="card-size">
+              <Card.Img variant="top" height="190px" src={service.pic} />
               <Card.Body>
                 <Card.Title>{service.name}</Card.Title>
                 <Card.Text>{service.about}</Card.Text>
-                <button className='btn btn-css' >Enroll Now</button>
+                <button className="btn btn-css">Enroll Now</button>
               </Card.Body>
             </Card>
           ))}
@@ -84,107 +84,148 @@ const Home = () => {
       </section>
       {/* our instractor....... */}
 
-      <section className='container'>
-        <h1 className='text-center pt-5'>Our Instructors</h1>
-        <hr className='w-25 m-auto my-3  c-tomato fw-bolder' />
-        <h4 className='text-center pb-5'>Meet Our Awesome & Creative Team</h4>
+      <section className="container">
+        <h1 className="text-center pt-5">Our Instructors</h1>
+        <hr className="w-25 m-auto my-3  c-tomato fw-bolder" />
+        <h4 className="text-center pb-5">Meet Our Awesome & Creative Team</h4>
 
-        <div className='img-css pb-5'>
-            
-            <div className='team1'>  
-                <h5  className='img-h'>Mohammad Kabir </h5>  
-            </div>
-            <div className='team2'>  
-                <h5  className='img-h'>Gopche suriya </h5>  
-            </div>
-            <div className='team3'>  
-                <h5  className='img-h'>Johan roy</h5>  
-            </div>
-            <div className='team4'>  
-                <h5 className='img-h'> Akbor Ali </h5>  
-            </div>
-            
-            
+        <div className="img-css pb-5">
+          <div className="team1">
+            <h5 className="img-h">Mohammad Kabir </h5>
+          </div>
+          <div className="team2">
+            <h5 className="img-h">Gopche suriya </h5>
+          </div>
+          <div className="team3">
+            <h5 className="img-h">Johan roy</h5>
+          </div>
+          <div className="team4">
+            <h5 className="img-h"> Akbor Ali </h5>
+          </div>
         </div>
-
       </section>
-{/* Our Features */}
-      <section className='container py-5'>
-        <h1 className='text-center py-3'>Our Features</h1>
-        <hr className='w-25 m-auto my-2  c-tomato fw-bolder' />
-        <h5 className='text-center py-5'>Why You Should Purchase This Template</h5>
+      {/* Our Features */}
+      <section className="container py-5">
+        <h1 className="text-center py-3">Our Features</h1>
+        <hr className="w-25 m-auto my-2  c-tomato fw-bolder" />
+        <h5 className="text-center py-5">
+          Why You Should Purchase This Template
+        </h5>
 
-        <div className='feature-main py-5'>
+        <div className="feature-main py-5">
+          <div className="feature">
+            <div className="px-3 ">
+              <FontAwesomeIcon
+                className="fs-1 c-tomato"
+                icon={faLaptop}
+              ></FontAwesomeIcon>
+            </div>
 
-          <div className='feature'>
-            <div className='px-3 '>
-         <FontAwesomeIcon className='fs-1 c-tomato' icon={faLaptop}></FontAwesomeIcon>
-         </div>
-          
-          <div>
-            <h3>Learn Online</h3>
-            <hr className='w-25  my-2  c-tomato fw-bolder' />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer lorem quam, adipiscing vel condimentum tristique. Read More</p>
+            <div>
+              <h3>Learn Online</h3>
+              <hr className="w-25  my-3  c-tomato fw-bolder" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer
+                lorem quam, adipiscing vel condimentum tristique. Read More
+              </p>
+            </div>
           </div>
+          <div className="feature">
+            <div className="px-3">
+              <FontAwesomeIcon
+                className="fs-1 c-tomato"
+                icon={faUsers}
+              ></FontAwesomeIcon>
+            </div>
+
+            <div>
+              <h3>Expert Professors</h3>
+              <hr className="w-25  my-3  c-tomato fw-bolder" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer
+                lorem quam, adipiscing vel condimentum tristique. Read More
+              </p>
+            </div>
           </div>
-          <div className='feature'>
-          <div className='px-3'>
-         <FontAwesomeIcon className='fs-1 c-tomato' icon={faUsers}></FontAwesomeIcon>
-         </div>
-          
-          <div>
-            <h3>Expert Professors</h3>
-            <hr className='w-25  my-2  c-tomato fw-bolder' />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer lorem quam, adipiscing vel condimentum tristique. Read More</p>
+          <div className="feature">
+            <div className="px-3">
+              <FontAwesomeIcon
+                className="fs-1 c-tomato"
+                icon={faFilm}
+              ></FontAwesomeIcon>
+            </div>
+
+            <div>
+              <h3>Video & Audio Courses</h3>
+              <hr className="w-25  my-3  c-tomato fw-bolder" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer
+                lorem quam, adipiscing vel condimentum tristique. Read More
+              </p>
+            </div>
           </div>
+          <div className="feature">
+            <div className="px-3">
+              <FontAwesomeIcon
+                className="fs-1 c-tomato"
+                icon={faChartLine}
+              ></FontAwesomeIcon>
+            </div>
+
+            <div>
+              <h3>Communicate With People</h3>
+              <hr className="w-25  my-3  c-tomato fw-bolder" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer
+                lorem quam, adipiscing vel condimentum tristique. Read More
+              </p>
+            </div>
           </div>
-          <div className='feature'>
-          <div className='px-3'>
-         <FontAwesomeIcon className='fs-1 c-tomato' icon={faFilm}></FontAwesomeIcon>
-         </div>
-          
-          <div>
-            <h3>Video & Audio Courses</h3>
-            <hr className='w-25  my-2  c-tomato fw-bolder' />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer lorem quam, adipiscing vel condimentum tristique. Read More</p>
+          <div className="feature">
+            <div className="px-3">
+              <FontAwesomeIcon
+                className="fs-1 c-tomato"
+                icon={faCertificate}
+              ></FontAwesomeIcon>
+            </div>
+
+            <div>
+              <h3>Trusted Certifications</h3>
+              <hr className="w-25  my-3  c-tomato fw-bolder" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer
+                lorem quam, adipiscing vel condimentum tristique. Read More
+              </p>
+            </div>
           </div>
+          <div className="feature">
+            <div className="px-3">
+              <FontAwesomeIcon
+                className="fs-1 c-tomato"
+                icon={faBook}
+              ></FontAwesomeIcon>
+            </div>
+
+            <div>
+              <h3>Professional Courses</h3>
+              <hr className="w-25  my-3  c-tomato fw-bolder" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer
+                lorem quam, adipiscing vel condimentum tristique. Read More
+              </p>
+            </div>
           </div>
-          <div className='feature'>
-          <div className='px-3'>
-         <FontAwesomeIcon className='fs-1 c-tomato' icon={faChartLine}></FontAwesomeIcon>
-         </div>
-          
-          <div>
-            <h3>Communicate With People</h3>
-            <hr className='w-25  my-2  c-tomato fw-bolder' />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer lorem quam, adipiscing vel condimentum tristique. Read More</p>
-          </div>
-          </div>
-          <div className='feature'>
-          <div className='px-3'>
-         <FontAwesomeIcon className='fs-1 c-tomato' icon={faCertificate}></FontAwesomeIcon>
-         </div>
-          
-          <div>
-            <h3>Trusted Certifications</h3>
-            <hr className='w-25  my-2  c-tomato fw-bolder' />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer lorem quam, adipiscing vel condimentum tristique. Read More</p>
-          </div>
-          </div>
-          <div className='feature'>
-          <div className='px-3'>
-         <FontAwesomeIcon className='fs-1 c-tomato' icon={faBook}></FontAwesomeIcon>
-         </div>
-          
-          <div>
-            <h3>Professional Courses</h3>
-            <hr className='w-25  my-2  c-tomato fw-bolder' />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiing elit. Integer lorem quam, adipiscing vel condimentum tristique. Read More</p>
-          </div>
-          </div>
+        </div>
+      </section>
+      <section className='last py-5'>
+        <div className='text-center container text-white pt-5 mt-5'>
+        <img src={pp} alt="img" />
+        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quis nihil deserunt odit laudantium, ad est reiciendis aliquid labore magnam optio facere et accusamus numquam! Recusandae iste placeat sapiente aut!</h4>
+        <h5> Md Habib <br /> - CEO , Ed Teach Ltd. </h5>
 
         </div>
-        
+
+
       </section>
     </div>
   )
